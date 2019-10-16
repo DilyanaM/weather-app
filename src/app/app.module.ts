7cc9ba3c2ef1171cf958835691718f38a6bcbc1c
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-10-16 13:23:59 +0300

    header; toggle-switch; add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { ToggleSwitchComponent } from './components/common/toggle-switch/toggle-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToggleSwitchComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
