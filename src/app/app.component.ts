9c536f96816931cc8eb9a8e431ab6a0f17e0a724
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-10-16 20:27:58 +0300

    pass theme data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+
+  title: string = 'Weather App';
+  darkModeActive: boolean;
+  darkModeSubscription;
+
+  constructor(public theme: ThemeService) {}
+
+  ngOnInit() {
+    this.darkModeSubscription =
+      this.theme.darkMode.subscribe(value => this.darkModeActive = value);
+  }
+
+  ngOnDestroy() {
+    this.darkModeSubscription.unsubscribe();
+  }
 }
